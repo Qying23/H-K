c98340b7bc41213c396599cb741ebe5cf29fa0a8
[ayseuluc] <you@example.com>
2018-05-23 15:01:52 +0200

    last version 1.1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-// update footer copyright year
+// // update footer copyright year
 
-var today = new Date();
-var year = today.getFullYear();
+// var today = new Date();
+// var year = today.getFullYear();
 
-var copyright = document.getElementById("copyright");
-copyright.innerHTML = '© Ayse Uluc, Ying Qi, Cornelia Steinkelner '+ year;
+// var copyright = document.getElementById("copyright");
+// copyright.innerHTML = '© Ayse Uluc, Ying Qi, Cornelia Steinkelner '+ year;
